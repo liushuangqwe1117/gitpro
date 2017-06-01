61f58a58046e82e3c5aeb8e89ad2be9c75cbbcbe
liushuang <liushuangqwe1117@163.com>
2017-06-01 14:14:35 +0800

    edit

--- Hello.java
+++ Hello.java
@@ -1,11 +1,8 @@
 public class Hello {
 	public static void main(String args[]) {
 		System.out.println("hello world!")
-<<<<<<< HEAD
-		print();
-=======
 		System.out.println("shiro test!")
->>>>>>> brh
+		print();
 	}
 	
 	public static void print(){
