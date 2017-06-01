5bd0931f61edca92c053fb599d663a6889ab033e
liushuang <liushuangqwe1117@163.com>
2017-06-01 11:37:36 +0800

    add method print

--- Hello.java
+++ Hello.java
@@ -1,5 +1,14 @@
 public class Hello {
 	public static void main(String args[]) {
 		System.out.println("hello world!")
+		System.out.println("spring test!")
+		print();
 	}
+	
+	public static void print(){
+		System.out.println("=================");
+		System.out.println("=== Hello Git===");
+		System.out.println("=================");
+	}
+	
 }
