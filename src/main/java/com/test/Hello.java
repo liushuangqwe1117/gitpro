81783c6e9c9dd1c1173146c49f30b5ac522d381d
liushuang <liushuangqwe1117@163.com>
2017-06-02 10:28:35 +0800

    b

--- src/main/java/com/test/Hello.java
+++ src/main/java/com/test/Hello.java
@@ -5,6 +5,13 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello git");
 		System.out.println("start git learn");
+		System.out.println("aaaaaaaaaaaaa");
+		print();
+	}
+	public static void print(){
+		System.out.println("===========================");
+		System.out.println("======== conflict test========");
+		System.out.println("===========================");
 	}
 	
 }
